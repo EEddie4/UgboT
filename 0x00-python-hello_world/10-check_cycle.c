b79c8987cafc7e082457177a8f1ca1dabc11687e
ugoMusk <ugobothankgod@gmail.com>
2022-08-29 20:54:17 +0100

    Optimize a program

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-#imclude <stdio.h>
+#include <stdio.h>
 
 /**
  * check_cycle - checks if a linked list contains a cycle
@@ -7,24 +7,20 @@
  *
  * Return: 1 if the list has a cycle, 0 if it doesn't
  */
-int main ()
+int check_cycle(listint_t *list)
 {
-  int check_cycle();
-}
-  int check_cycle(listint_t *list)
-  {
-    listint_t *first_node = list;
-    listint_t *second_node = list;
-    if (!list)
-      return (0);
-
-    while (first_node && second_node && second_node->next)
-      {
-	first_node = first_node->next;
-	second_node = second_node->next->next;
-	if (first_node == second_node)
-	  return (1);
-      }
-
+  listint_t *first_node = list;
+  listint_t *second_node = list;
+  if (!list)
     return (0);
-  }
+
+  while (first_node && second_node && second_node->next)
+    {
+      first_node = first_node->next;
+      second_node = second_node->next->next;
+      if (first_node == second_node)
+	return (1);
+    }
+
+  return (0);
+}
