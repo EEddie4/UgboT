cb3d8d01eeac936b1e7438c358c172d953090e40
ugoMusk <ugobothankgod@gmail.com>
2022-08-29 20:22:26 +0100

    Optimize a program

--- 0x00-python-hello_world/10-check_circle.c
+++ 0x00-python-hello_world/10-check_circle.c
@@ -1,26 +1,29 @@
 #include "lists.h"
 
 /**
- * check_cycle - checks if a linked list contains a cycle
+ * check_circle - checks if a linked list contains a cycle
  * @list: linked list to check
  *
  * Return: 1 if the list has a cycle, 0 if it doesn't
  */
-int check_cycle(listint_t *list)
+int main ()
 {
-	listint_t *first_node = list;
-	listint_t *second_node = list;
-
-	if (!list)
-		return (0);
-
-	while (first_node && second_node && second_node->next)
-	{
-		first_node = first_node->next;
-		second_node = second_node->next->next;
-		if (first_node == second_node)
-			return (1);
-	}
-
-	return (0);
+  int check_circle();
 }
+  int check_circle(listint_t *list)
+  {
+    listint_t *first_node = list;
+    listint_t *second_node = list;
+    if (!list)
+      return (0);
+
+    while (first_node && second_node && second_node->next)
+      {
+	first_node = first_node->next;
+	second_node = second_node->next->next;
+	if (first_node == second_node)
+	  return (1);
+      }
+
+    return (0);
+  }
