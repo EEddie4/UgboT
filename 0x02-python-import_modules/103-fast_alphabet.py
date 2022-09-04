9e4137323c302b25835a4c582a15cf2342d09939
ugoMusk <ugobothankgod@gmail.com>
2022-09-04 22:30:41 +0100

    importing and using functions and creating modules in Python.

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author: ThankGod Ugobo
+
+import string
+print(string.ascii_uppercase)
---
+++ 0x02-python-import_modules/variable_load_5.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author: ThankGod Ugobo
+
+a = 98
+"""Simple variable
+"""
